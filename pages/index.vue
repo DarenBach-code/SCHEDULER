4814c01d91d1cc10b3bfd6fd3bae4e15ac431789
Daren <darenbach2020@gmail.com>
2024-08-22 22:52:31 -0500

    things

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,11 @@
             <h1 class="rubik text-2xl flex-auto"><span class="text-orange-400">My</span>Scheduler</h1>
             
             <button class="font-sans font-bold px-2"><a href="#">Log in</a></button>
-            <button class="font-sans font-bold px-2"><a href="#">Sign up</a></button>      
+            <button class="bg-black text-white rounded-full font-sans font-bold px-4 py-2"><a href="#">Sign up</a></button>      
+        </div>
+        <div class="flex px-8 py-8">
+            <img class="w-[30rem] h-[30rem]" src="/public/schedulerpic.svg" alt="image of schedule">
+            <h1>things</h1>
         </div>
     </div>
 </template>
