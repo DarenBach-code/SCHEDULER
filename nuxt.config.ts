a0f18ec28c0e2dd266e4e2c101328aa143a3f67b
Daren <darenbach2020@gmail.com>
2024-08-27 15:03:19 -0500

    supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/supabase"
+  ],
   googleFonts: {
     families: {
       "Rubik Glitch": true,
